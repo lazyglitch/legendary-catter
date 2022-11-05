86805b0d339676b15c9597727fdfde80c8b39784
lazyglitch <hehh999@inbox.ru>
2022-11-05 23:24:08 +0700

    completed todo.01

---
+++ Todo/Program.cs
@@ -0,0 +1,214 @@
+﻿
+using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using TodoModels;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.Unicode;
+
+Logic logic = new Logic();
+
+Console.WriteLine("Hi!");
+
+bool stat = true;
+
+while(stat){
+
+    Console.WriteLine("0-Закрыть\n1-Войти\n2-Регистрация");
+
+    int num_s;
+
+    string? answ = Console.ReadLine();
+
+    while (!Int32.TryParse(answ, out num_s) || num_s <0 || num_s >2)
+    {
+            RedConsole("Invalid input");
+
+            answ = Console.ReadLine();
+    }
+
+    switch(answ){
+
+        case "0":
+            stat=false;
+            break;
+        case "1":
+            if(Login()){
+                stat=false;
+            }
+            break;
+        case "2":
+            if(Signup()){
+                stat=false;
+            }
+            break;
+    }
+}
+
+void Start(string name){
+
+    bool onLoad = true;
+
+    while(onLoad){
+        Console.WriteLine("0-Выход\n1-Добавить\n2-Удалить\n3-Вывести список\n4-Редактировать");
+        
+        int num;
+
+        string? answ = Console.ReadLine();
+
+        while (!Int32.TryParse(answ, out num) || num <0 || num>4)
+        {
+                RedConsole("Invalid input");
+
+                answ = Console.ReadLine();
+        }
+
+        switch(answ){
+
+            case "0":
+                onLoad=false;
+                break;
+            case "1":
+                AddNew();
+                break;
+            case "2":
+                Delete();
+                break;
+            case "3":
+                GetNotes();
+                break;
+            case "4":
+                Edit();
+                break;
+        }
+    }
+}
+
+void GetNotes(){    
+    Console.Clear();
+    
+    var notes = logic.GetAllNotes();
+    
+    if(notes.Count==0)
+    {
+        Console.WriteLine("Список пуст...\n");
+    }
+
+    int i =0;
+
+    foreach(Note n in notes)
+    {
+        string status = n.Status ? "Выполняется" : "Завершено";
+
+        Console.WriteLine($"{i}) {n.Content} ({status})");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+void Edit(){
+
+    GetNotes();
+
+    Console.WriteLine("Выберите запись для редактирования\nВведите id:");
+
+    int ex;
+    var answ = Console.ReadLine();
+
+    while (String.IsNullOrWhiteSpace(answ) || !Int32.TryParse(answ, out ex) || ex <0 || ex>logic.GetAllNotes().Count)
+    {
+        RedConsole("Invalid input");
+
+        answ = Console.ReadLine();
+    }
+    Console.WriteLine("Если изменения не требуются нажмите пробел");
+    
+    Console.WriteLine("Введите новый текст:");
+    
+    string text = Console.ReadLine();
+
+    Console.WriteLine("Введите статус\n0-завершено\n1-выполняется");
+
+    string status  = Console.ReadLine();
+
+    logic.EditNote(Int32.Parse(answ), text, status);
+
+}
+
+void Delete(){
+    
+    GetNotes();
+
+    Console.WriteLine("Введите id:");
+
+    int ex;
+
+    string answ = Console.ReadLine();
+
+    while (!Int32.TryParse(answ, out ex) || ex <0 || ex>logic.GetAllNotes().Count)
+    {
+        RedConsole("Invalid input");
+
+        answ = Console.ReadLine();
+    }
+
+    logic.DeleteNote(Int32.Parse(answ));
+    
+}
+
+void AddNew(){
+
+    Console.WriteLine("Введите текст:");
+
+    string text = Console.ReadLine();
+
+    logic.AddNewNote(text);
+}
+
+bool Login(){
+    
+    Console.WriteLine("Введите имя:");
+
+    string name = Console.ReadLine();
+
+    if(logic.LogIn(name) && !String.IsNullOrWhiteSpace(name))
+    {
+        Console.WriteLine("Пользователь авторизирован!");
+
+        Start(name);
+
+        return true;
+    }
+    RedConsole("Incorrect data");
+    return false;
+}
+
+bool Signup(){
+
+    Console.WriteLine("Введите имя:");
+
+    string name = Console.ReadLine();
+
+    if(logic.Registration(name) && !String.IsNullOrWhiteSpace(name))
+    {
+        Console.WriteLine("Успех!");
+
+        logic.LogIn(name);
+
+        Start(name);
+
+        return true;
+    }
+    RedConsole("Такой пользователь уже есть");
+    return false;
+}
+
+static void RedConsole(string text)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+
+    Console.WriteLine(text);
+
+    Console.ResetColor();
+}
---
+++ TodoLogic/Logic.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Reflection.Metadata;
+using System.Text;
+
+using TodoModels;
+
+namespace TodoLogic{}
+public class Logic
+{
+    private static string path = @"..\Storage\";
+    private string currentUser {get; set;} 
+    private List<string> users = new List<string>();
+    private List<Note> UserNotes = new List<Note>();
+    public Logic()
+    {
+        using(StreamReader sr = File.OpenText("..\\Storage\\users.txt"))
+        {
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                users.Add(s);
+            }
+        }
+    }
+    public bool Registration(string name)
+    {
+        if(users.Contains(name))
+        {
+            return false;
+        }
+
+        Directory.CreateDirectory(Path.Combine(path, name));
+
+
+       using (StreamWriter s = File.AppendText(Path.Combine(path, "users.txt")))
+            {
+                s.WriteLine(name);
+            } 
+
+        users.Add(name);
+
+        return true;
+    }
+    public bool LogIn(string name){
+        
+        if(users.Contains(name) && Directory.Exists(Path.Combine(path,name)))
+        {
+            currentUser = name;
+
+            path = Path.Combine(path, name);
+
+            NotesToList();
+
+            return true;
+        }
+        return false;
+    }
+    public void AddNewNote(string text)
+    {
+        Note note = new Note(text, currentUser, "active_"+Guid.NewGuid().ToString(), true);
+
+        UserNotes.Add(note);
+
+        using (StreamWriter fs = File.CreateText(Path.Combine(path,$"{note.Title}.txt")))
+        {    
+              fs.WriteLine(text);    
+        }
+    }
+    public void DeleteNote(int id)
+    {
+        Note note = UserNotes[id];
+        
+        if (File.Exists(Path.Combine(path, $"{note.Title}.txt")))
+        {
+            File.Delete(Path.Combine(path, $"{note.Title}.txt"));
+        }
+        UserNotes.RemoveAt(id);
+    }
+    public List<Note> GetAllNotes()
+    {
+        NotesToList();
+        return UserNotes;
+    }
+    public void EditNote(int id, string text, string status)
+    {
+        string n_title = UserNotes[id].Title;
+        
+        if(!String.IsNullOrWhiteSpace(text))
+        {
+            using (StreamWriter fs = File.CreateText(Path.Combine(path,$"{n_title}.txt")))
+            {    
+                fs.WriteLine(text);    
+            } 
+        }
+        if(!String.IsNullOrWhiteSpace(status)){
+
+            string stat = status == "1" ? "active": "completed";
+
+            FileInfo fi = new FileInfo(Path.Combine(path,$"{n_title}.txt"));
+            string pref =  n_title.Substring(n_title.LastIndexOf("_")+1);
+            fi.MoveTo(Path.Combine(path,$"{stat}_{pref}.txt"));
+        }
+
+    }
+    public void NotesToList()
+    {
+        UserNotes = new List<Note>();
+
+        string[] filePaths = Directory.GetFiles(path, "*.txt");
+
+        if(filePaths.Length == 0) return;
+
+        foreach(string f in filePaths)
+        {
+            string fPath = f.Substring(path.Length + 1);
+
+            string fName = fPath.Substring(0, fPath.LastIndexOf("."));
+
+            string fStatus = fPath.Substring(0, fPath.LastIndexOf("_"));
+
+            string content = "";
+            
+            using (StreamReader sr = File.OpenText(f))    
+            {    
+                string s = "";    
+                while ((s = sr.ReadLine()) != null)    
+                {    
+                    content+=s;   
+                }    
+            }
+            bool status = fStatus == "completed"? false : true;
+
+            Note note_to_add = new Note(content, currentUser,fName , status);
+
+            UserNotes.Add(note_to_add);
+        }
+    }
+}
+
