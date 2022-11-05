441338715b277ce4881f95e2d1a71ff468c96f88
lazyglitch <hehh999@inbox.ru>
2022-11-05 23:45:45 +0700

    edited

---
+++ TodoModels/Note.cs
@@ -0,0 +1,17 @@
+﻿namespace TodoModels;
+public class Note
+{
+    public string User {get;}
+    public string Title {get; set;}
+    public string Content {get; set;}
+    public bool Status {get; set;}
+    public Note(){}
+    public Note(string text, string user, string title, bool status)
+    {
+        User=user;
+        Content=text;
+        Title= title;
+        Status = status;
+    }
+
+}
